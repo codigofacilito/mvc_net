654e071e0edc3a4c5b369062358df2047cc6467f
Marcos Hernandez <marclinux@yahoo.com>
2017-03-13 19:03:07 -0600

    Agregue el script de los roles

---
+++ BaseDatos/Roles.sql
@@ -0,0 +1,4 @@
+INSERT [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'8f85590c-1648-4595-83f0-adddc59bd39d', N'Admin')
+INSERT [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'8a03b90b-830c-4ea4-92aa-ef8350a5f40c', N'AdminAgenda')
+INSERT [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'e45fe678-e65d-4f76-aa62-57689399f06b', N'AdminClientes')
+INSERT [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'84bb6aa9-2aea-4f8b-9a91-716b794f1fcc', N'Operador')
