f39f978e2018e9647f1fed8d374990b8592e2ba9
Marcos Hernandez <marclinux@yahoo.com>
2018-02-13 18:10:55 -0600

    Agregue lo de WEB API

---
+++ CRMFacilitoInicial/CRMFacilitoInicial/Controllers/ConsultasController.cs
@@ -0,0 +1,45 @@
+﻿using CRMFacilitoInicial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace CRMFacilitoInicial.Controllers
+{
+    public class ConsultasController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public IEnumerable<Cliente> Get(string inicio)
+        {
+            var consulta = from c in db.Clientes
+                           where c.Nombre.Contains(inicio)
+                           select c;
+            return consulta.ToList();
+        }
+
+        public Cliente Get(int id)
+        {
+            Cliente cliente = new Cliente();
+            cliente = db.Clientes.Find(id);
+            if(cliente != null)
+            {
+                db.Entry(cliente).Collection("Direcciones").Load();
+                db.Entry(cliente).Collection("Telefonos").Load();
+                db.Entry(cliente).Collection("Correos").Load();
+            }
+            return cliente;
+        }
+
+        public void Put(Cliente cliente)
+        {
+            Cliente clienteDb = db.Clientes.Find(cliente.ClienteId);
+            if(clienteDb != null)
+            {
+                clienteDb.TipoClienteId = cliente.TipoClienteId;
+                db.Entry(clienteDb).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+    }
+}
